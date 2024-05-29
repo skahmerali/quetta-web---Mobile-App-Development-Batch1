35ac4e68d001dd0c8d93591d93c1bf04761bc6ea
ahmer-styre <ahmer.styre@gmail.com>
2024-05-29 23:59:41 +0500

    javascript classes

---
+++ js/class 1/app.js
@@ -0,0 +1,47 @@
+// var let const
+// Javascript is a case sensitive
+// var name = 'ahmer';
+// name = 'ali';
+
+// console.log(firstName2);
+
+// var firstName2 = 'ahmer' ; //string datatype
+// var firstName2 = 'ahmer' ; //string datatype
+// alert("hello this is our user name :" + firstName2);
+
+// var firstName = prompt("please insert your first name :");
+// alert("hello : " + firstName)
+
+// var firstNumber = '045';
+// var secondNumber = 20;
+
+// alert(firstNumber + secondNumber);
+// var firstNumber =+ prompt("Enter your first number");
+// var secondNumber =+ prompt("Enter your second number");
+// alert(firstNumber + secondNumber);
+// var firstNumber = Number(prompt("Enter your first number"));
+// var secondNumber = Number(prompt("Enter your second number"));
+// alert("Addition ==> " + firstNumber + secondNumber);
+// alert("Multiplication ==> " + firstNumber * secondNumber);
+// alert("Devision ==> " + firstNumber / secondNumber);
+// alert("Subtraction ==> " + (firstNumber - secondNumber));
+
+
+// alert((2 + (3 * (8 / 2))) - 7); // right to left 
+// alert(((2 + 3) * 8) / (2 - 7)); // right to left 
+
+
+// var number;
+// number = 1;
+// number++ ; //2 Post Icreament
+// ++number ; //3 preIncreament 
+
+// console.log(--number);
+// console.log(number);
+var userName = prompt('enter ur name ');
+if (userName === 'ali') {
+    alert("Hello " + userName + " you area a valid user !")
+}else{
+    alert("sorry " + userName + " you are not a valid user !")
+
+}
---
+++ js/class 3/app.js
@@ -0,0 +1,69 @@
+// var firstName = prompt('Enter your name');
+// var secondName = prompt('Enter your second name')
+
+// if (firstName === 'ahmer') {
+//     alert("Congrats you are an actual person")
+// } else if (firstName === 'ali') {
+//     alert("Congrats you are an actual person")
+// }else {
+//     alert('you are a wrong person')
+// }
+
+// if(firstName === 'ahmer' || firstName === 'ali'  ){
+//     alert("Congrats you are an actual person")
+// }else{
+//     alert('you are a wrong person')
+// }
+// if (firstName === 'ahmer' && secondName === 'ali') {
+//     alert("Congrats you are an actual person")
+// } else {
+//     alert('you are a wrong person')
+// }
+
+// if (firstName === 'sheikh') {
+//     if (secondName === 'ali') {
+//         alert("Congrats you are an actual person")
+
+//     }else{
+
+//         alert("you made a mistake in second name")
+//     }
+// }else {
+//     alert('you made a mistake in first name')
+// }
+var table = prompt('insert ur desire number to print a table')
+// var email = prompt('insert ur invalid email')
+
+var users = ['ahmer@gmail.com', 'ahmer1@gmail.com', 'ahmer2@gmail.com'];
+// alert(users[1])
+// alert(users)
+// alert(users.length)
+users.push('ahmer3@gmail.com')
+// alert(users)
+// users.pop()
+// users.pop()
+// users.pop()
+// users.pop()
+// users.unshift(1, true)
+// users.shift()
+// users.splice(3, 1, 'hello Quetta')
+// const newArrays = users.slice(0, 4)
+// alert(users)
+// alert(newArrays)
+// for (var i = 0; i < users.length; i++ ){
+//     // alert(users[i])
+//     if(email === users[i]){
+//         alert('you are a valid user')
+//     }else{
+//         alert('you are not a valid user')
+//     }
+// }
+
+
+// for (var i = 20; i <= 200; i++) {
+//     console.log(table + ' * ' + i, '=', table * i)
+// }
+
+for (var i = 10; i > 0; i--){
+    console.log(i);
+}
